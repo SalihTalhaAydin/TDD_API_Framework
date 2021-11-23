d0f0be3604a2d2e572818b942f1fb2207f9c1066
Salih Aydin <salihtalha19991@gmail.com>
2021-11-23 15:33:18 -0600

    Added necessary utilities

---
+++ src/test/java/utils/ConfigReader.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties properties;
+
+    static {
+        //Configuration file path
+        String path = "configuration.properties";
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(fileInputStream);
+        }catch (Exception e ){
+            System.out.println("Error! The config file is not found. " +
+                    "{Check the path or the file existing or not}" +
+                    "Message: " + e.getMessage());
+        }
+    }
+
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
